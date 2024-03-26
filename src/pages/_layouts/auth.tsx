3887b53506e3d197b6388db3cf53aa7fe9d6c1ff
Lucas-M01 <lucas.montenegro.n@outlook.com>
2024-03-26 15:07:06 -0300

    feat: create sign-in

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,12 +1,21 @@
 import { Outlet } from 'react-router-dom'
 
+import { Header } from '@/components/header'
+
 export function AuthLayout() {
   return (
     <div>
-      <h1>Autentificação</h1>
+      <Header />
+      <div className="container relative hidden min-h-screen flex-col items-center justify-center antialiased md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <div className="relative hidden h-full flex-col border-r border-foreground/5 bg-muted p-10 text-muted-foreground dark:border-r lg:flex">
+          <div className="flex h-full items-center justify-center gap-3 text-7xl font-medium text-foreground">
+            <span className="font-bold">UAI Cupcakes</span>
+          </div>
+        </div>
 
-      <div>
-        <Outlet />
+        <div className="relative flex flex-col items-center justify-center">
+          <Outlet />
+        </div>
       </div>
     </div>
   )
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,97 @@
-import { Helmet } from 'react-helmet-async'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { Link, useSearchParams } from 'react-router-dom'
+import { toast } from 'sonner'
+import { z } from 'zod'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+const signInSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+})
+
+type SignInSchema = z.infer<typeof signInSchema>
 
 export function SignIn() {
+  const [searchParams] = useSearchParams()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInSchema>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: searchParams.get('email') ?? '',
+    },
+  })
+
+  async function handleAuthenticate({ email, password }: SignInSchema) {
+    try {
+      toast.success('Enviamos um link de autenticação para seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleAuthenticate({ email, password }),
+        },
+      })
+    } catch (err) {
+      toast.error('Credenciais inválidas')
+    }
+  }
+
   return (
-    <div>
-      <Helmet title="Login" />
-      <h1>SignIn</h1>
+    <div className="lg:p-8">
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="flex flex-col space-y-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">Login</h1>
+          <p className="text-sm text-muted-foreground">
+            Faça login para poder comprar seus cupcakes!
+          </p>
+        </div>
+
+        <div className="grid gap-6">
+          <form onSubmit={handleSubmit(handleAuthenticate)}>
+            <div className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Seu e-mail</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  autoCapitalize="none"
+                  autoComplete="email"
+                  autoCorrect="off"
+                  {...register('email')}
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Sua senha</Label>
+                <Input
+                  id="password"
+                  type="password"
+                  {...register('password')}
+                />
+                <Link
+                  to="/forgot-password"
+                  className="text-sm text-muted-foreground hover:text-foreground"
+                >
+                  Esqueci minha senha
+                </Link>
+              </div>
+              <Button type="submit" disabled={isSubmitting}>
+                Acessar painel
+              </Button>
+              <Link to="/sign-up">
+                <Button className="w-full" variant="outline">
+                  Cadastrar
+                </Button>
+              </Link>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
